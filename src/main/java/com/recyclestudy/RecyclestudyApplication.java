8be040566e455c185ce3574513010fbb633314e8
jhan0121 <jhan0121@gmail.com>
2025-12-21 20:38:47 +0900

    init: 프로젝트 초기 설정 추가

---
+++ src/main/java/com/recyclestudy/RecyclestudyApplication.java
@@ -0,0 +1,13 @@
+package com.recyclestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecyclestudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecyclestudyApplication.class, args);
+	}
+
+}
