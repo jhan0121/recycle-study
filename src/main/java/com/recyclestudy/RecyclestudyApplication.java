3f403b4ee8de8fc5f1476481e949ade7f4290080
jhan0121 <jhan0121@gmail.com>
2025-12-23 23:46:20 +0900

    feat: RecyclestudyApplication에 비동기 설정 추가

--- src/main/java/com/recyclestudy/RecyclestudyApplication.java
+++ src/main/java/com/recyclestudy/RecyclestudyApplication.java
@@ -2,12 +2,14 @@ package com.recyclestudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class RecyclestudyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RecyclestudyApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RecyclestudyApplication.class, args);
+    }
 
 }
