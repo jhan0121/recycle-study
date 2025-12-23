d38ad9b023e59a2e4bcc07faa4172353e9804b66
jhan0121 <jhan0121@gmail.com>
2025-12-23 23:48:11 +0900

    feat: 전역 예외 처리용 ControllerAdvice 추가

---
+++ src/main/java/com/recyclestudy/exception/BadRequestException.java
@@ -0,0 +1,7 @@
+package com.recyclestudy.exception;
+
+public class BadRequestException extends RuntimeException {
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/recyclestudy/exception/GlobalControllerAdvice.java
@@ -0,0 +1,35 @@
+package com.recyclestudy.exception;
+
+import com.recyclestudy.exception.dto.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalControllerAdvice {
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNotFound(final NotFoundException e) {
+        final ErrorResponse response = ErrorResponse.from(e.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ErrorResponse> handleBadRequest(final BadRequestException e) {
+        final ErrorResponse response = ErrorResponse.from(e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<ErrorResponse> handleUnauthorized(final UnauthorizedException e) {
+        final ErrorResponse response = ErrorResponse.from(e.getMessage());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(final Exception e) {
+        final ErrorResponse response = ErrorResponse.from("예기치 못한 에러가 발생했습니다");
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+    }
+}
