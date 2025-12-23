76b70be496a4c0aa146644b0aabc98e5bf8bff46
jhan0121 <jhan0121@gmail.com>
2025-12-23 20:12:22 +0900

    feat: Email 추가

---
+++ src/main/java/com/recyclestudy/member/domain/Email.java
@@ -0,0 +1,43 @@
+package com.recyclestudy.member.domain;
+
+import com.recyclestudy.common.NullValidator;
+import jakarta.persistence.Embeddable;
+import java.util.regex.Pattern;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldNameConstants;
+
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@FieldNameConstants(level = AccessLevel.PRIVATE)
+@Getter
+@EqualsAndHashCode
+public class Email {
+
+    private static final String EMAIL_FORMAT = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+    private static final Pattern EMAIL_PATTERN = Pattern.compile(EMAIL_FORMAT);
+
+    public static Email from(final String value) {
+        validateNotNull(value);
+        validateEmailFormat(value);
+        return new Email(value);
+    }
+
+    private static void validateNotNull(final String value) {
+        NullValidator.builder()
+                .add(Fields.value, value)
+                .validate();
+    }
+
+    private static void validateEmailFormat(final String emailValue) {
+        if (!EMAIL_PATTERN.matcher(emailValue).matches()) {
+            throw new IllegalArgumentException("유효하지 않은 이메일 형식입니다.");
+        }
+    }
+
+    private String value;
+}
