fce2ce9723f3637e662301098624c420380b6996
jhan0121 <jhan0121@gmail.com>
2025-12-24 13:35:23 +0900

    feat: Device 소유 검증 기능 추가

--- src/main/java/com/recyclestudy/member/domain/Device.java
+++ src/main/java/com/recyclestudy/member/domain/Device.java
@@ -2,6 +2,7 @@ package com.recyclestudy.member.domain;
 
 import com.recyclestudy.common.BaseEntity;
 import com.recyclestudy.common.NullValidator;
+import com.recyclestudy.exception.BadRequestException;
 import jakarta.persistence.AttributeOverride;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
@@ -10,6 +11,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -27,13 +29,15 @@ public class Device extends BaseEntity {
     public static Device withoutId(
             final Member member,
             final DeviceIdentifier deviceIdentifier,
-            final boolean isActive
+            final boolean isActive,
+            final ActivationExpiredDateTime activationExpiresAt
     ) {
         NullValidator.builder()
                 .add(Fields.member, member)
                 .add(Fields.identifier, deviceIdentifier)
+                .add(Fields.activationExpiresAt, activationExpiresAt)
                 .validate();
-        return new Device(member, deviceIdentifier, isActive);
+        return new Device(member, deviceIdentifier, isActive, activationExpiresAt);
     }
 
     @ManyToOne(fetch = FetchType.LAZY)
@@ -47,7 +51,18 @@ public class Device extends BaseEntity {
     @Column(name = "is_active", nullable = false)
     private boolean isActive;
 
-    public void activate() {
+    @Embedded
+    @AttributeOverride(name = "value", column = @Column(name = "activation_expires_at", nullable = false))
+    private ActivationExpiredDateTime activationExpiresAt;
+
+    public void activate(final LocalDateTime currentTime) {
+        activationExpiresAt.checkExpired(currentTime);
         this.isActive = true;
+    }
+
+    public void verifyOwner(final Email email) {
+        if (!member.hasEmail(email)) {
+            throw new BadRequestException("디바이스 소유자가 아닙니다.");
+        }
     }
 }
