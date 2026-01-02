f67a53a0bac452f6905ae046e92bfc2fd8ee5a02
jhan0121 <56645802+jhan0121@users.noreply.github.com>
2026-01-02 21:24:15 +0900

    flyway 기반 db 마이그레이션 의존성 추가 (#24)
    * feat: flyway 기반 db 마이그레이션 의존성 추가
    
    - 환경별 jpa sql 출력 여부 분리
    
    * fix: ReviewCycle#scheduledAt not null 누락 수정
    
    * test: 테스트 환경에서 flyway 비활성화
    
    * 로그 기능 추가 (#21)
    
    * feat: 로그 기능 추가
    
    * chore: 신규 유저 이메일 등록 시작 로그 태그명 수정
    
    * feat: 이메일 마스킹 기능 적용
    
    * refactor: 복습 주기 저장 로그 포맷 수정
    
    * refactor: 이메일 전송 기능 도메인 객체 파라미터로 변경
    
    * test: MemberServiceTest#authenticateDevice 테스트 커버리지 보완 (#22)

---
+++ src/main/resources/db/migration/V20260102_1__init.sql
@@ -0,0 +1,60 @@
+-- member
+create table member
+(
+    id          bigint auto_increment primary key,
+    email       varchar(255) not null,
+    created_at  datetime(6)  not null,
+    modified_at datetime(6)  null,
+    constraint uk_member_email unique (email)
+);
+
+-- device
+create table device
+(
+    id                    bigint auto_increment primary key,
+    identifier            varchar(255) not null,
+    is_active             bit          not null,
+    activation_expires_at datetime(6)  not null,
+    member_id             bigint       not null,
+    created_at            datetime(6)  not null,
+    modified_at           datetime(6)  null,
+    constraint uk_device_identifier unique (identifier),
+    constraint fk_device_member_id
+        foreign key (member_id) references member (id)
+);
+
+-- review
+create table review
+(
+    id          bigint auto_increment primary key,
+    url         text        not null,
+    member_id   bigint      not null,
+    created_at  datetime(6) not null,
+    modified_at datetime(6) null,
+    constraint fk_review_member_id
+        foreign key (member_id) references member (id)
+);
+
+-- review_cycle
+create table review_cycle
+(
+    id           bigint auto_increment primary key,
+    review_id    bigint      not null,
+    scheduled_at datetime(6) not null,
+    created_at   datetime(6) not null,
+    modified_at  datetime(6) null,
+    constraint fk_review_cycle_review_id
+        foreign key (review_id) references review (id)
+);
+
+-- notification_history
+create table notification_history
+(
+    id              bigint auto_increment              primary key,
+    review_cycle_id bigint                             not null,
+    created_at      datetime(6)                        not null,
+    modified_at     datetime(6)                        null,
+    status          enum ('FAILED', 'PENDING', 'SENT') not null,
+    constraint fk_notification_history_review_cycle_id
+        foreign key (review_cycle_id) references review_cycle (id)
+);
